013040d00ac502c32303ccb66d3ea0f5c2ba8e4b
pasqwal <courty.pascal@gmail.com>
2021-12-01 11:38:20 +0100

    New flask project boilerplate

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-docstring
+
+from flask import Flask
+from flask_restx import Api
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/hello')
+    def hello():
+        return "Hello World!"
+
+    from .apis.tweets import api as tweets
+    api = Api()
+    api.add_namespace(tweets)
+    api.init_app(app)
+
+    app.config['ERROR_404_HELP'] = False
+
+    return app
